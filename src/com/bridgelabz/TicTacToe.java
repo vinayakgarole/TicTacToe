e8e212f77ff66c1669e22c11324b67b64dc0e8f0
vinayakgarole <vinayakgarole10@gamil.com>
2023-02-14 17:21:01 +0530

    Modified: Create board

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -2,8 +2,18 @@ package com.bridgelabz;
 
 public class TicTacToe {
 
+    static char[] board = new char[10];
+
+    static void createBoard(char[] board) {
+
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to TicTacToe Program");
+        createBoard(board);
     }
 }
